fd035126b1e2af40d56ff3d38aff6cc8a90738a6
RakhinH <rakhinchowdhury@icloud.com>
2024-10-23 12:54:58 +0100

    cxxx

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+
+const App = () => {
+    return ( 
+        <>
+        <div className=" flex justify-center items-center bg-center w-full h-screen bg-[url('https://iongroup.com/wp-content/uploads/2024/03/The-rise-and-future-of-credit-portfolio-trading.png')]" >
+            {/* <img className="" src="https://iongroup.com/wp-content/uploads/2024/03/The-rise-and-future-of-credit-portfolio-trading.png"/> */}
+        
+            <p className="box-decoration-slice bg-gradient-to-r from-indigo-300 to-indigo-900 text-black px-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, quibusdam?
+        </p>
+        </div>
+        
+        </>
+     );
+}
+ 
+export default App;
+
+/*
+width: 100
+*/
