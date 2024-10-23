5a4fe6c72b6ed2e77fa96b8cfc0cfdb5169bad62
RakhinH <rakhinchowdhury@icloud.com>
2024-10-23 11:05:53 +0100

    xdws

---
+++ web_story/src/App.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class App extends Component {
+    render() {
+        const myStyle = {
+            backgroundImage: "",
+            height: "100vh",
+            marginTop: "-70px",
+            fontSize: "50px",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+        };
+        return (
+            <div style={myStyle}>
+                <h1> geeksforgeeks </h1>
+            </div>
+        );
+    }
+}
+
+export default App;
