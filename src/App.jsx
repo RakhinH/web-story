2b47376d9133c4d9e3055abd0cc11f70149c24b3
RakhinH <rakhinchowdhury@icloud.com>
2024-11-05 10:55:14 +0000

    21

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,24 @@ import React, { Component } from "react";
 
 
 const App = () => {
+    const handleClick=() => {
+        // function: to go to next page
+    };
+
     return ( 
         <>
         <div className=" flex justify-center items-center bg-center w-full h-screen bg-[url('https://iongroup.com/wp-content/uploads/2024/03/The-rise-and-future-of-credit-portfolio-trading.png')]" >
-            {/* <img className="" src="https://iongroup.com/wp-content/uploads/2024/03/The-rise-and-future-of-credit-portfolio-trading.png"/> */}
-        
-            <p className="box-decoration-slice bg-gradient-to-r from-indigo-300 to-indigo-900 text-black px-2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, quibusdam?
-        </p>
+            <div className="box-decoration-slice bg-gradient-to-r from-indigo-300 to-indigo-900 text-black px-2 w-2/3 flex flex-col items-center">
+                <p>
+                    It is 2008 and and the stockmarket
+                </p>
+
+                <div className=" flex justify-center gap-9 w-fit">
+                    <button className="border-2 border-violet-600 bg-violet-600" type="button" onClick={handleClick}>Option 1</button>
+                    <button className="border-2 border-violet-600 bg-violet-600" type="button" onClick={handleClick}>Option 2</button>
+                </div>
+                
+            </div>             
         </div>
         
         </>
