c82ed81d97249063e011f7bf6809d99e6788bf48
RakhinH <rakhinchowdhury@icloud.com>
2024-11-06 09:27:00 +0000

    jyjy

--- src/components/base.jsx
+++ src/components/base.jsx
@@ -19,12 +19,14 @@ const Story = () => {
         <>
         {current && (
             <>
-            <div className="box-decoration-slice bg-gradient-to-r from-indigo-300 to-indigo-900 text-black px-2 w-2/3 flex flex-col items-center">
-                <p>{current.text}</p>
+            <div className="bg-[url('https://iongroup.com/wp-content/uploads/2024/03/The-rise-and-future-of-credit-portfolio-trading.png')]">
+                <div className="box-decoration-slice bg-gradient-to-r from-indigo-300 to-indigo-900 text-black px-2 w-2/3 flex flex-col items-center">
+                    <p>{current.text}</p>
 
-                {current.buttons.map((b) => {
-                    <Link to={"/story/" + b.id}>{b.text}</Link>
-                })}
+                    {current.buttons.map((b) => {
+                        <Link to={"/story/" + b.id}>{b.text}</Link>
+                    })}
+                </div>
             </div>
             </>
         )}
