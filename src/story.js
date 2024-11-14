a1ddc4cb4cddf860672e77fbe9b9ad06e70a078c
RakhinH <rakhinchowdhury@icloud.com>
2024-11-14 18:54:47 +0000

    Checked story and added more detail

--- src/story.js
+++ src/story.js
@@ -15,6 +15,8 @@
 
 
 export const story = {
+    // Spelling checked: Done
+    // Code Working: Yes 
     "first": {
         text: "It is 2008 and you're in a trading company, you have two major options to invest in, one could lead to your boss getting angry at you, or anything else could happen.",
         image: "https://iongroup.com/wp-content/uploads/2024/03/The-rise-and-future-of-credit-portfolio-trading.png",
@@ -30,10 +32,11 @@ export const story = {
         ]
     },
 
-    
-
+    // Spelling checked: Done
+    // Code Working: Yes 
     "b": {
-        text: "You invested in apple and 10 years later John Pork has arrived at ur door step, he has asked you to do a task. He gives you a piece of chicken as an incentive.  The task that john pork has asked you to do is, to find a skibidi sigma male with 5 gyatt rizz ohio level",
+        
+        text: "You invested in apple and 10 years later John Pork has arrived at your door step, he has asked you to do a task. He gives you a piece of chicken as an incentive.  The task that john pork has asked you to do is, to find a skibidi sigma male with 5 gyatt rizz ohio level",
         image: "https://i.ytimg.com/vi/9o3HYDqOleM/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLC8APTklJIGFIR1r64sM5ffTqINog",
         buttons: [
             {
@@ -47,25 +50,10 @@ export const story = {
         ]
     },
 
-    
-
-    "bad": {
-        text: "John pork kills you and Ninja with a low taper fade falls down a cliff and dies. ",
-        image: "https://i.ytimg.com/vi/MGs__rwhoEA/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDgdfll_saZi9XUZAap2xYxrDN6yA",
-        buttons: [
-            {
-                text: "",
-                id: "bad"
-            },
-            {
-                text1: "",
-                id1: ""
-            }
-        ]
-    },
-
+    // Spelling checked: Done
+    // Code Working: Yes 
     "b2": {
-        text: "You are walking through the woods to complete the task and all of a sudden a dragon appears and a man in a top hat is riding on the back, u follow him then all of a sudden a portal spawns behind you and u slowly look behind and its the same dragon and wizard flying in the sky. The wizard introduces himself as Sir Bumbahole Boris and invites u to his humble abode. You enter the Wizards house, he trys to cook you a meal but doesnt have the right ingredients. He asks u to accompany him on a jorney to get ingredients.",
+        text: "You are walking through the woods to complete the task and all of a sudden a dragon appears and a man in a top hat is riding on the back, you follow him then all of a sudden a portal spawns behind you and you slowly look behind and it was the same dragon and wizard flying in the sky. The wizard introduces himself as Sir Bumbahole Boris and invites you to his humble abode. You enter the Wizards house, he trys to cook you a meal but he doesnt have the right ingredients. He asks you to accompany him on a journey to get the ingredients.",
         image: "https://is1-ssl.mzstatic.com/image/thumb/Music124/v4/83/61/d5/8361d52e-77aa-f683-4aaf-a81d08298d55/195497933754.jpg/1200x1200bb.jpg",
         buttons: [
             {
@@ -79,23 +67,27 @@ export const story = {
         ]
     },
 
+    // Spelling checked: Done
+    // Code Working: Yes 
     "b3": {
-        text: "He walks away and then u find wat john pork was looking for, the man speaks to u and says 'who u?' u reply 'i work for john pork and i need to bring you in' the man says no, so u hit him the head. and put him in your car. he wakes up and starts shouting why he should be let go, u start listening and find out that john pork has his family. what do you do? If you save his family you will die for being disloyal ",
+        text: "He walks away and then you find wat john pork was looking for, the man speaks to you and says 'who you bwoy?' you reply 'i work for john pork and i need to bring you in' the man says no, so you hit him the head, and put him in your car. He wakes up and starts shouting why he should be let go, you start listening and find out that john pork has his family. what do you do? If you save his family you may die for being disloyal ",
         image: "https://c8.alamy.com/comp/B7AXED/man-putting-body-in-trunk-of-car-B7AXED.jpg",
         buttons: [
             {
-                text: "Save his family",
+                text: "Save his family, and hand him in",
                 id: "true"
             },
             {
-                text1: "Save yourself",
+                text1: "Save yourself, and don't hand him in",
                 id1: "bad2"
             }
         ]
     },
 
+    // Spelling checked: Done
+    // Code Working: Yes 
     "b4": {
-        text: "You go to get ingredients and u find a homeless dude, he asking if u and the wizard will give him a piece of chicken. You say 'Thats such a  weird request' but luckily u had a piece of chicken from john pork",
+        text: "You go to get ingredients and you find a homeless dude, he asking if you and the wizard will give him a piece of chicken. You say 'Thats such a weird request' but luckily you had a piece of chicken from john pork",
         image: "https://www.shutterstock.com/image-photo/homeless-city-man-asking-food-260nw-1611745312.jpg",
         buttons: [
             {
@@ -109,10 +101,10 @@ export const story = {
         ]
     },
 
-    
-
+    // Spelling checked: Done
+    // Code Working: Yes 
     "b6": {
-        text: "He takes it and runs off, and u and wizard walk off. You and the wizard find the ingredients inside a samll shack, you ask 'Are we stealing this ?' he replies with 'No'. You and the wizard are having a nice chat and litch ten mins away from his hosue and then all of a sudden a bullet goes through the wizards head, you look dumbfounded for a second and the u turn back...",
+        text: "He takes it and runs off, and you and wizard walk off. You and the wizard find the ingredients inside a small shack, you ask 'Are we stealing this ?' he replies with 'No'. You and the wizard are having a nice chat and you're literally ten minutes away from his house and then all of a sudden, a bullet goes whizzing past , you look dumbfounded for a second and then you turn back your eyes open in shock its...",
         image: "https://thumbs.dreamstime.com/b/dumbfounded-7452720.jpg",
         buttons: [
             {
@@ -129,8 +121,10 @@ export const story = {
 
 // the 'a' options are below
 
+    // Spelling checked: Done
+    // Code Working: Yes 
     "a": {
-        text: "You to Eling Mosque to pray and elon musk with a tusk appears.He asks u to talk to his daughter, she is a vsco girl who came up to u saying 'its giving...sksksksk' she asks u to 'find a meme that could living rent-free in a simp'",
+        text: "5 years later, you go to Eling Mosque to pray and elon musk with a tusk appears.He asks you to talk to his daughter, she is a vsco girl who came up to you saying 'its giving...sksksksk' she asks you to 'find a person called Frank Scaiff'.",
         image: "https://i.pinimg.com/736x/a3/4b/bb/a34bbb10c7136f0624f1c92ad4044ea7.jpg",
         buttons: [
             {
@@ -144,10 +138,10 @@ export const story = {
         ]
     },
     
-    
-
+    // Spelling checked: Done
+    // Code Working: Yes 
     "a2": {
-        text: "You walk through the streets on bristol and go to house 40 on Donald Rd, you find a piece of text saying, 'i am at access creative college, i think ed sheeran has forced me to play the guitar do you think u could play the guitar to get me back?'. What do you do?",
+        text: "You walk through the streets on bristol and go to house 40 on Donald Rd, you find a piece of text saying, 'i am at access creative college, and i think ed sheeran has forced me to play the guitar do you think you could play the guitar to get me back?'. What do you do?",
         image: "https://i.ytimg.com/vi/IQxYKXwFZbM/hqdefault.jpg",
         buttons: [
             {
@@ -163,6 +157,8 @@ export const story = {
 
     // the different endings are below
 
+    // Spelling checked: Done
+    // Code Working: Yes 
     "bad": {
         text: "John Pork gets angry and Shrek comes out of his swamp and kills you.",
         image: "https://platform.polygon.com/wp-content/uploads/sites/2/chorus/uploads/chorus_asset/file/22512212/shrek4_disneyscreencaps.com_675.jpg?quality=90&strip=all&crop=44.127604166667,30.392156862745,36.953125,57.96568627451",
@@ -178,6 +174,8 @@ export const story = {
         ]
     },
 
+    // Spelling checked: Done
+    // Code Working: Yes 
     "bad1": {
         text: "He gets angry and chases you out town, so keep on running out fear and run into a ditch and starve to death",
         image: "https://lh3.googleusercontent.com/proxy/pEWhMpW625_TgjfWBeCaaXFdZWSmN3sPgdxLLQDSe-0OS0AZnagaBRUVuADmMItI7sj7QUA98IEl1gXY9GB6UrnMK0A18J5fwRTZ17TdA6vN6b8ENVcG3RjDC_As",
@@ -193,8 +191,10 @@ export const story = {
         ]
     },
 
+    // Spelling checked: Done
+    // Code Working: Yes 
     "bad2": {
-        text: "his family dies and you feel so much regret that you do sucide. (PLEASE NOTE THAT KILLING YOURSELF IS NOT THE ANSWER AND IF YOU HAVE THOUGHTS OF SUCIDE CALL THIS LINE: 116 123 )",
+        text: "His family dies and you feel so much regret that you do sucide. (PLEASE NOTE THAT KILLING YOURSELF IS NOT THE ANSWER AND IF YOU HAVE THOUGHTS OF SUCIDE CALL THIS LINE: 116 123 )",
         image: "https://mappingignorance.org/app/uploads/2014/06/Freedom-and-the-regret-connection.jpg",
         buttons: [
             {
@@ -208,8 +208,10 @@ export const story = {
         ]
     },
 
+    // Spelling checked: Done
+    // Code Working: Yes 
     "bad3": {
-        text: "She goes and says 'i dont know' you say 'I dont think you should not NOT help me.' then she says 'i will kill you' and then you are killed by a bullet. ",
+        text: "She say 'help me or i will kill you' you say 'no'  and then you are killed by a Thanos who t-bags your body. ",
         image: "https://img.redro.pl/obrazy/pistolety-pistolety-karabiny-maszyny-maszynowe-rewolwery-i-strzelby-z-czarna-szczegolowa-sylwetka-pojedynczo-na-bialym-tle-wektor-zestaw-ikon-broni-i-broni-palnej-vol-2-400-151612664.jpg",
         buttons: [
             {
@@ -223,8 +225,10 @@ export const story = {
         ]
     },
 
+    // Spelling checked: Done
+    // Code Working: Yes 
     "bad4": {
-        text: "Ed Sheeran got angry and killed the person John Pork was looking for. So then u walk home and John pork kills you. And just before you die he whispers to you 'you are a bumbahole' ",
+        text: "Ed Sheeran got angry and killed the person John Pork was looking for. So then you walk home and John pork kills you. And just before you die he whispers to you 'you are a narcistic bumbahole' ",
         image: "https://images.fastcompany.com/image/upload/f_webp,q_auto,c_fit/fc/3051160-poster-p-1-this-companion-app-lets-a-friend-virtually-walk-you-home-at-night.jpg",
         buttons: [
             {
@@ -238,8 +242,10 @@ export const story = {
         ]
     },
 
+    // Spelling checked: Done
+    // Code Working: Yes 
     "true": {
-        text: "You save his family and it turns out he is a billionaire so he protects you for the rest of his life so John Pork cant kill u. ",
+        text: "You save his family and it turns out he is a billionaire so he protects you for the rest of his life so John Pork cant kill you. ",
         image: "https://media.npr.org/assets/img/2015/10/16/pork_custom-a8c4aa18a06dd590f3d8a57e677edc09c046f68e.jpg?s=1100&c=85&f=jpeg",
         buttons: [
             {
@@ -253,8 +259,10 @@ export const story = {
         ]
     },
 
+    // Spelling checked: Done
+    // Code Working: Yes 
     "true1": {
-        text: "Its a cliffhangr of course you are not menat to know, silly boy you thought.",
+        text: "Its a cliffhangr of course you are not meant to know, silly boy you thought.",
         image: "https://avantecture.com/wp-content/uploads/2023/08/CLIFFHANGER-1.jpg",
         buttons: [
             {
@@ -268,8 +276,10 @@ export const story = {
         ]
     },
 
+    // Spelling checked: Done
+    // Code Working: Yes 
     "good": {
-        text: "U played the guitar and ed sheeran dies. You go return the thing john pork asked for and he gives a billion pound. ",
+        text: "You played the guitar and ed sheeran dies. You go return the thing john pork asked for and he gives a billion pound. ",
         image: "https://timandjulieharris.com/wp-content/uploads/2019/10/rich-people.jpg",
         buttons: [
             {
